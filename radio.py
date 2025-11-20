b9a10046f608c00b1db5270c5b107314639ab682
cris482 <crumfaker02@gmil.com>
2025-11-20 21:32:16 +0700

    RADIO PRO 1 SORONG -102.6 FM

---
+++ radio.py
@@ -0,0 +1,52 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+# --- URL RADIO ----
+RADIO_URL = "https://stream-node1.rri.co.id/streaming/17/9217/rrisorongpro1.mp3"
+
+
+
+@bot.event
+async def on_ready():
+    print(f"Bot online sebagai {bot.user}")
+
+
+@bot.command()
+async def radio(ctx):
+    """Play radio di voice channel."""
+    if not ctx.author.voice:
+        return await ctx.send("❌ Kamu harus join voice channel terlebih dahulu.")
+
+    channel = ctx.author.voice.channel
+
+    if ctx.voice_client is None:
+        await channel.connect()
+
+    voice = ctx.voice_client
+
+    if voice.is_playing():
+        voice.stop()
+
+    voice.play(discord.FFmpegPCMAudio(
+        RADIO_URL,
+        before_options="-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5"
+    ))
+
+    await ctx.send("🎶 Radio RRI mulai diputar!")
+
+
+@bot.command()
+async def stop(ctx):
+    """Stop radio & bot keluar."""
+    if ctx.voice_client:
+        await ctx.voice_client.disconnect()
+        await ctx.send("⛔ Radio dihentikan, bot keluar.")
+    else:
+        await ctx.send("Bot tidak ada di voice channel.")
+
+
+bot.run("ISI TOKEN DISCORD DISINI")
